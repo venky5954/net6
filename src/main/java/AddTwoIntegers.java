39414f2f89a47fca668edc179d562934940b69f4
venky5954 <59354036+venky5954@users.noreply.github.com>
2020-01-31 20:19:35 +0530

    Update AddTwoIntegers.java

--- src/main/java/AddTwoIntegers.java
+++ src/main/java/AddTwoIntegers.java
@@ -1,9 +1,18 @@
-public class AddTwoIntegers {
+public class Palindrome {
     public static void main(String[] args) {
-        
-        int first = 10;
-        int second = 20;
-        int sum = first + second;
-        System.out.println("The sum is: " + sum);
+        int num = 121, reversedInteger = 0, remainder, originalInteger;
+        originalInteger = num;
+        // reversed integer is stored in variable 
+        while( num != 0 )
+        {
+            remainder = num % 10;
+            reversedInteger = reversedInteger * 10 + remainder;
+            num  /= 10;
+        }
+        // palindrome if orignalInteger and reversedInteger are equal
+        if (originalInteger == reversedInteger)
+            System.out.println(originalInteger + " is a palindrome.");
+        else
+            System.out.println(originalInteger + " is not a palindrome.");
     }
 }
