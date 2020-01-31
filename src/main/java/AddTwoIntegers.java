4e33dafae81c8a4455ea57b8d9ad29dcfb5bd170
venky5954 <59354036+venky5954@users.noreply.github.com>
2020-01-31 20:18:47 +0530

    Update AddTwoIntegers.java

--- src/main/java/AddTwoIntegers.java
+++ src/main/java/AddTwoIntegers.java
@@ -1,9 +1,11 @@
-public class AddTwoIntegers {
+public class Pattern {
     public static void main(String[] args) {
-        
-        int first = 10;
-        int second = 20;
-        int sum = first + second;
-        System.out.println("The sum is: " + sum);
+        int rows = 5;
+        for(int i = 1; i <= rows; ++i) {
+            for(int j = 1; j <= i; ++j) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
