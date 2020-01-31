5f2cf75873af105aefbd3a73ace2d6743da0fa7e
venky5954 <59354036+venky5954@users.noreply.github.com>
2020-01-31 22:10:16 +0530

    Update AddTwoIntegers.java

--- src/main/java/AddTwoIntegers.java
+++ src/main/java/AddTwoIntegers.java
@@ -1,4 +1,4 @@
-public class Fibonacci {
+public class AddTwoIntegers {
     public static void main(String[] args) {
         int n = 10, t1 = 0, t2 = 1;
         System.out.print("First " + n + " terms: ");
